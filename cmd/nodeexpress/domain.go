66ef308788e451a941373c3f7b9bf7b56c043fa2
Vedashree Patil <vedapatil@deloitte.nl>
2022-05-03 16:45:25 +0200

    domain.go updated

--- cmd/nodeexpress/domain.go
+++ cmd/nodeexpress/domain.go
@@ -1,11 +1,9 @@
 package nodeexpress
 
 import (
-	"fmt"
 	"os"
 	"path/filepath"
 	templates "stew/pkg/templates/nodeexpress"
-	"strings"
 )
 
 var domainSettings = templates.DomainTemplate{AppName: "", DirectoryPath: ""}
@@ -17,40 +15,20 @@ func AddModel(appName string, domains []string) error {
 		addRoutes(modelName)
 		addSchema(modelName)
 	}
-
-	var httpMethods = []string{"post", "put", "get", "delete"}
-	for _, method := range httpMethods {
-		err = addModelFile(domain+"."+method, "test")
-		if err != nil {
-			fmt.Printf("Errored out %s", err)
-
-			return err
-		}
-	}
-
 	return nil
 }
 
 func addControllers(modelName string) error {
 	// Get path to add the model
 	currentDir, _ := os.Getwd()
-	domainSettings.DirectoryPath = filepath.Join(currentDir, fileType, modelName+"s")
-	err := os.MkdirAll(domainSettings.DirectoryPath, os.ModePerm)
-	templateName := templates.NodeExpressRouteTemplate
-	var method string
-	switch fileType {
-	case "route":
-		templateName = templates.NodeExpressRouteTemplate
-	case "controller":
-		templateName = templates.NodeExpressControllerTemplate
-	case "schema":
-		templateName = templates.NodeExpressModelTemplate
-	case "test":
-		templateName = templates.NodeExpressTestTemplate
-		splitModelName := strings.Split(modelName, ".")
-		modelName = splitModelName[0]
-		method = splitModelName[1]
+	// parse
+	domainSettings.DirectoryPath = filepath.Join(currentDir, "app", "controllers", modelName+"s")
+	domainSettings.TemplateName = templates.NodeExpressControllerTemplate
+	domainSettings.DomainName = modelName
 
+	err := templates.AddNodeExpressapiTemplate(domainSettings)
+	if err != nil {
+		return err
 	}
 
 	return nil
@@ -78,7 +56,6 @@ func addRoutes(modelName string) error {
 	domainSettings.DirectoryPath = filepath.Join(currentDir, "app", "routes", modelName+"s")
 	domainSettings.TemplateName = templates.NodeExpressRouteTemplate
 	domainSettings.DomainName = modelName
-	domainSettings.Method = method
 	// parse
 	err := templates.AddNodeExpressapiTemplate(domainSettings)
 	if err != nil {
